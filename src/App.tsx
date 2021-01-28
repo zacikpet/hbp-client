a4c343c7f2e321e6a7fbe37999977443071f4607
Peter Žáčik <zacikpet@fit.cvut.cz>
2021-01-28 10:46:26 +0100

    Eslint and prettier setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import CircleMenu from './components/CircleMenu'
+
+const App: FC = () => {
+  return (
+    <div id="app">
+      <CircleMenu />
+    </div>
+  )
+}
+
+export default App
