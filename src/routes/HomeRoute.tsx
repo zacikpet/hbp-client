8050157404cc9bb83772bef11df0912ea9b1fc02
Peter Žáčik <zacikpet@fit.cvut.cz>
2021-04-30 17:57:56 +0200

    Update introduction

--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -8,11 +8,10 @@ const HomeRoute: FC = () => {
     <div className="min-h-page">
       <div className="shadow bg-gradient-to-b from-white to-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-gray-850 rounded-b mb-2">
         <Gallery
-          title="HBP – the portal for categorised information about the Higgs boson"
-          text="The Higgs boson, named after the the physicist Peter Higgs is a subatomic particle. It is the manifestation of the Higgs field, a quantum field that is present everywhere throughout the Universe.
-        Certain particles interact with the Higgs field field via the Higgs mechanism and as a result obtain mass. The Higgs boson is the result of an excitation in this field."
-          source="CERN 2021"
-          more="https://en.wikipedia.org/wiki/Higgs_boson"
+          title="Higgs Boson Portal (HBP) – a portal for up-to-date categorised articles about the Higgs boson"
+          text="The HBP offers complete information about the latest and historic articles to experts. The categorisation is according to preliminary and published results. Using artifical intelligence, it also offers an automated categorisation for specific production and decay modes of the Higgs boson. The corresponding database is updated daily."
+          // text="The Higgs boson, named after the the physicist Peter Higgs is a subatomic particle. It is the manifestation of the Higgs field, a quantum field that is present everywhere throughout the Universe. Certain particles interact with the Higgs field field via the Higgs mechanism and as a result obtain mass. The Higgs boson is the result of an excitation in this field."
+          more="https://home.cern/science/physics/higgs-boson"
           contentClassName="w-full lg:w-3/4"
           contentLabel="Illustration of a particle collision producing the Higgs boson"
           contentSource="Lucas Taylor, CMS"
